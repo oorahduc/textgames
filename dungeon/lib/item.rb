8d0fca77c702ff595b104afbb3342eaf9d1d6b22
Chris Kimmel <chriskimmel@gmail.com>
2016-01-15 10:06:49 -0800

    meh

--- dungeon/lib/item.rb
+++ dungeon/lib/item.rb
@@ -23,20 +23,6 @@ class Item
   end
 end
 
-# class Item
-#   attr_accessor :name, :type, :attack, :armor, :wearloc, :weight, :price
-#   def initialize(object)
-#     @name = object['name']
-#     @type = object['type']
-#     @attack = object['attack']
-#     @armor = object['armor']
-#     @wearloc = object['wearloc']
-#     @weight = object['weight']
-#     @price = object['price']
-#   end
-# end
-
-
 class Potion < Item
   def initialize
     @name = "potion"
